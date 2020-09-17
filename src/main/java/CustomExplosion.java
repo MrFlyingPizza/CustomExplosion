a83ce227ed7dd8670b1b7fbf3af2f36d25b7b1d8
MrFlyingPizza <65780477+MrFlyingPizza@users.noreply.github.com>
2020-09-16 17:27:19 -0700

    Initial commit

---
+++ src/main/java/CustomExplosion.java
@@ -0,0 +1,22 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomExplosion extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        System.out.println("Custom Explosion loaded.");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        System.out.println("Custom Explosion disabled.");
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        System.out.println("Custom Explosion enabled.");
+        getServer().getPluginManager().registerEvents(new ExplosionListener(), this);
+    }
+}
---
+++ src/main/java/ExplosionListener.java
@@ -0,0 +1,39 @@
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
+import org.bukkit.util.Vector;
+
+public class ExplosionListener implements Listener {
+
+    @EventHandler
+    public void onExplosion(EntityExplodeEvent event) {
+        System.out.println("Explosion event listened.");
+        System.out.println(event.getEntityType());
+
+        Location eventLocation = event.getLocation();
+        World eventWorld = eventLocation.getWorld();
+        eventWorld.playSound(eventLocation, Sound.ENTITY_GENERIC_EXPLODE, 5, 5);
+        eventWorld.playEffect(eventLocation, Effect.SMOKE, 5);
+
+        event.setCancelled(true);
+
+        double explosionPower = 3;
+        double xOffset, yOffSet, zOffset;
+        for (Block block : event.blockList()) {
+
+            xOffset = eventLocation.getX() - block.getX();
+            yOffSet = eventLocation.getY() - block.getY();
+            zOffset = eventLocation.getZ() - block.getZ();
+
+            FallingBlock fallingBlock = eventWorld.spawnFallingBlock(block.getLocation(),block.getBlockData());
+            block.setType(Material.AIR);
+            fallingBlock.setVelocity(new Vector(explosionPower/xOffset,explosionPower/yOffSet,explosionPower/zOffset));
+        }
+    }
+}
